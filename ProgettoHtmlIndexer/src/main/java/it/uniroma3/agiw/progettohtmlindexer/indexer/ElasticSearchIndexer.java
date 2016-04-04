0482ab6cbcc5274ddf37769d13ff00493e6c727c
Daniela <Daniela@10.25.2.30>
2016-04-05 00:00:30 +0200

    fixes vari, aggiunta classe per json

---
+++ ProgettoHtmlIndexer/src/main/java/it/uniroma3/agiw/progettohtmlindexer/indexer/ElasticSearchIndexer.java
@@ -0,0 +1,180 @@
+package it.uniroma3.agiw.progettohtmlindexer.indexer;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.tika.io.IOUtils;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+
+import com.google.gson.Gson;
+
+import it.uniroma3.agiw.progettohtmlindexer.model.ParsedResult;
+
+public class ElasticSearchIndexer {
+	
+
+	public ElasticSearchIndexer(){
+		super();
+	}
+	
+	private Client getClient(){
+			
+			Settings settings = Settings.settingsBuilder()
+					.put("cluster.name", "elasticsearch").build();
+			Client transportClient = TransportClient.builder()
+					.settings(settings)
+					.build();
+			
+			try{
+				
+				InetSocketTransportAddress inetSocketTransportAddress = 
+						new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300);
+				((TransportClient) transportClient).addTransportAddress(inetSocketTransportAddress);
+			
+			}catch(Exception e){
+				
+				e.printStackTrace();
+			}
+			
+			return (Client) transportClient;
+	}
+	
+	
+	public void index(ParsedResult parsedRes) throws IOException, InterruptedException {
+		
+			final Client client = getClient();
+			final String indexName = "agiw"; 
+			final String documentType = "html";
+	        
+	        final IndicesExistsResponse res = 
+	        		client.admin().indices().prepareExists(indexName).execute().actionGet();
+	        
+	        if (res.isExists()) {
+	            final DeleteIndexRequestBuilder delIdx = client.admin().indices().prepareDelete(indexName);
+	            delIdx.execute().actionGet();
+	        }
+
+	        final CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices().prepareCreate(indexName);
+			
+	     // MAPPING 
+
+	        final XContentBuilder mappingBuilder = jsonBuilder()
+	        		.startObject()
+	        			.startObject(documentType)
+	        				.startObject("properties")
+		        				.startObject("url")
+		        					.field("type", "string")
+		        					.field("store","yes")
+		        				.endObject()
+		        				.startObject("title")
+		        					.field("type", "string")
+		        					.field("store", "no")
+		        					.field("analyzer", "standard")
+		        				.endObject()
+		        				.startObject("content")
+		        					.field("type", "string")
+		        					.field("store", "no")
+		        					.field("analyzer", "CustomAnalyzer")
+		        				.endObject()
+		        			.endObject()
+	        			.endObject()
+	                .endObject();
+	        
+	        System.out.println(mappingBuilder.string());
+	        createIndexRequestBuilder.addMapping(documentType, mappingBuilder);
+
+	     // MAPPING DONE
+	        
+	     /* ANALYZER 
+	      * NB:per ora ho usato filter, tokenizer, etc del codice da cui ho preso ispirazione 
+	      * ma ce ne sono una valanga, bisogna decidere ql usare
+	      */
+	        final XContentBuilder settingsBuilder = XContentFactory.jsonBuilder()
+	        		.startObject()
+	                .startObject("analysis")
+	                	.startObject("char_filter") 
+	                		.startObject("filter_html")
+	                			.field("type", "html_strip") //rimuove tag html
+	                		.endObject()
+	                	.endObject()
+	                    .startObject("filter")
+	                        .startObject("filter_shingle")
+	                            .field("type","shingle")
+	                            .field("max_shingle_size",2)
+	                            .field("min_shingle_size",2)
+	                            .field("output_unigrams",false)
+	                        .endObject()
+	                        .startObject("filter_elision")
+	                        	.field("type","elision")
+	                        	.array("articles",
+	                        			"c", "l", "all", "dall", "dell",
+	                                    "nell", "sull", "coll", "pell",
+	                                    "gl", "agl", "dagl", "degl", "negl",
+	                                    "sugl", "un", "m", "t", "s", "v", "d")
+	                        .endObject()
+	                        .startObject("filter_stemmer")
+	                            .field("type","porter_stem")
+	                            .field("language","italian")
+	                        .endObject()
+	                        .startObject("filter_stop")
+	                        	.field("type","stop")
+	                        	.field("stopwords","_italian_")
+                            .endObject()
+	                    .endObject()
+	                    .startObject("tokenizer")
+	                        .startObject("my_ngram_tokenizer")
+	                            .field("type","nGram")
+	                            .field("min_gram",1)
+	                            .field("max_gram",1)
+	                        .endObject()
+	                    .endObject()
+	                    .startObject("analyzer")
+	                        .startObject("CustomAnalyzer")
+	                            .field("tokenizer","my_ngram_tokenizer")
+	                            .array("filter","standard","lowercase","asciifolding","filter_stemmer","filter_shingle","filter_stop","filter_elision")
+	                            .field("char_filter", "filter_html")
+	                        .endObject()
+	                    .endObject()
+	                .endObject()
+	            .endObject();
+	        
+	        System.out.println(settingsBuilder.string());
+	        createIndexRequestBuilder.setSettings(settingsBuilder);
+		// ANALYZER DONE
+	        
+	        final CreateIndexResponse createResponse = createIndexRequestBuilder.execute().actionGet();
+			System.out.println(createResponse.toString());
+	        
+			IndexRequestBuilder indexRequestBuilder = client.prepareIndex(indexName, documentType);
+	        
+			Gson input = new Gson();
+			System.out.println(input.toJson(parsedRes, ParsedResult.class).toString());
+	        indexRequestBuilder.setSource(input.toJson(parsedRes, ParsedResult.class).toString());
+	        IndexResponse response = indexRequestBuilder.execute().actionGet();	
+	        
+	        System.out.println(response.toString());
+	     	   
+	    	    }
+
+
+	}
+
+
