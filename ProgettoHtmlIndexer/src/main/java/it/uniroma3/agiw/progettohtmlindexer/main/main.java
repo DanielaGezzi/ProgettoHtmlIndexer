3808c73c505f7acb965cab490ff760f4740db43c
Daniela <Daniela@10.25.2.30>
2016-04-05 12:33:29 +0200

    fix

--- ProgettoHtmlIndexer/src/main/java/it/uniroma3/agiw/progettohtmlindexer/main/main.java
+++ ProgettoHtmlIndexer/src/main/java/it/uniroma3/agiw/progettohtmlindexer/main/main.java
@@ -1,27 +1,35 @@
 package it.uniroma3.agiw.progettohtmlindexer.main;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import it.uniroma3.agiw.progettohtmlindexer.indexer.ElasticSearchIndexer;
 import it.uniroma3.agiw.progettohtmlindexer.model.ParsedResult;
 
 public class main {
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) throws UnsupportedEncodingException{
 		
 		ElasticSearchIndexer esi = new ElasticSearchIndexer();
 		ParsedResult pr = new ParsedResult();
 		
 		pr.setUrl("www.prova.it");
-		pr.setTitle("provone della fattanza italiana");
-		pr.setContent("C’era una volta, nel cuor dell’inverno, mentre i fiocchi di neve cadevano dal cielo "
+		pr.setTitle("provolone della fattanza italiana");
+		
+		
+		String myString = "C’era una volta, nel cuor dell’inverno, mentre i fiocchi di neve cadevano dal cielo "
 				+ "come piume, una regina che cuciva, seduta accanto a una finestra dalla cornice di ebano. "
 				+ "E così, cucendo e alzando gli occhi per guardar la neve, si punse un dito, e caddero nella"
 				+ " neve tre gocce di sangue. Il rosso era così bello su quel candore, ch’ella pensò: "
 				+ "‘ Avessi una bambina bianca come la neve, rossa come il sangue e daincapelli neri come "
 				+ "il legno della finestra! ‘ Poco dopo diede alla luce una figlioletta bianca come la neve,"
 				+ " rossa come il sangue e dai capelli neri come l’ebano; e la chiamarono Biancaneve. "
-				+ "E quando nacque, la regina morì.");
+				+ "E quando nacque, la regina morì.";
+				
+				byte ptext[] = myString.getBytes();
+				String value = new String(ptext, "UTF-8");
+				
+		pr.setContent(value);
 		
 		
 		try {
